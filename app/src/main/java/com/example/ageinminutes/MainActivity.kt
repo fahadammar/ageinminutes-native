2c5055a63f4ad6183ae24d0f72ff7d80bc19f464
Fahad Ammar <fahadammar96@outlook.com>
2021-10-02 19:36:23 +0500

    last final commit for the update

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ageinminutes
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,75 +15,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnDatePicker.setOnClickListener {view ->
-            clickDataPicker(view)
-        //Toast.makeText(this, "Your Toast Here", Toast.LENGTH_LONG).show()
-        }
+       btnDatePicker.setOnClickListener { view ->
+           clickDatePicker(view)
+           //Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+       }
+
+
 
 
     }
 
     // Function
-    fun clickDataPicker(view: View){
-        val c = Calendar.getInstance()
+    fun clickDatePicker(view: View) {
+        val cal =  Calendar.getInstance();
+        val year = cal.get(Calendar.YEAR);
+        val month = cal.get(Calendar.MONTH);
+        val day = cal.get(Calendar.DAY_OF_MONTH);
 
-        // These are current, getting from your mobile phone.
-        // You can check the comments in the DOBCalculator from Course - Resources
-        var day = c.get(Calendar.DAY_OF_MONTH)
-        var month = c.get(Calendar.MONTH)
-        var year = c.get(Calendar.YEAR)
+        val dpd = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, dayOfMonth ->
+            Toast.makeText(this, "Year: $selectedYear, Month: $selectedMonth, Day: $dayOfMonth", Toast.LENGTH_LONG).show();
 
-        val dpd = DatePickerDialog(
-                this,
-                DatePickerDialog.OnDateSetListener {
-                    view, year, monthOfYear, dayOfMonth ->
+                val textForDate = "$dayOfMonth/$selectedMonth/$selectedYear"
 
-                    // In Java Month Is From 0-11, that's why we did +1 in it
-                    val selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
+                tvSelectedDate.setText(textForDate)
 
-                    // Now Make It Visible To The User
-                    tvSelectedDate.setText(selectedDate)
+                val simple_date_format = SimpleDateFormat("dd/mm/yyyy", Locale.ENGLISH)
 
-                    // Formatted Date
-                    var sdf =  SimpleDateFormat("dd/mm/yyy", Locale.ENGLISH)
+                // check the documentation for the SimpleDateFormat
+                val theDate = simple_date_format.parse(textForDate) // parse(text -> Date) - changed to Date
 
-                    // Parsing the formatted date
-                    val theDate = sdf.parse(selectedDate)
+                val selectedDateInMinutes = theDate!!.time / 60000 // converting the date into minutes
 
-                    // Current date in to minutes.
-                    val selectedDateToMinutes = theDate!!.time / 60000
+                // get the System Current time and then convert into the Date
+                val currentDate =  simple_date_format.parse(simple_date_format.format(System.currentTimeMillis()))
 
-                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
-                    // Current date in to minutes.
-                    val currentDateToMinutes = currentDate!!.time / 60000
+                val currentDateInMinutes = currentDate!!.time / 60000 // converting the date into minutes
 
-                    /**
-                     * Now to get the difference into minutes.
-                     * We will subtract the selectedDateToMinutes from currentDateToMinutes.
-                     * Which will provide the difference in minutes.
-                     */
-                    val differenceInMinutes = currentDateToMinutes - selectedDateToMinutes
+                val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
 
-                    // Set the difference in minutes to textview to show the user.
-                    tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+                tvSelectedDateInMinutes.setText(differenceInMinutes.toString())
 
-
-
-                }
-                , year,month,day
+        },
+            year, month, day
         )
-
-        /**
-         * Sets the maximal date supported by this in
-         * milliseconds since January 1, 1970 00:00:00 in time zone.
-         *
-         * @param maxDate The maximal supported date.
-         */
-        // 86400000 is milliseconds of 24 Hours. Which is used to restrict the user to select today or past and not future day.
+        // 86400000 is the milliseconds of the day
         dpd.datePicker.setMaxDate(Date().time - 86400000)
-        dpd.show() // It is used to show the datePicker Dialog.
-
-        // To Calculate in The Age in Day - Age/ 60 * 24
+        dpd.show()
     }
 
 
